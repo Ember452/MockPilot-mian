3feb8e3870cc44b8162d9a1e81fddf0c0ec8e8eb
Solis <16898659+epoch2@user.noreply.gitee.com>
2026-07-01 22:19:12 +0800

    - 答题轮次异常补偿机制

--- AI-Meeting-main/admin/src/main/java/com/hewei/hzyjy/xunzhi/common/constant/RedisCacheConstant.java
+++ AI-Meeting-main/admin/src/main/java/com/hewei/hzyjy/xunzhi/common/constant/RedisCacheConstant.java
@@ -24,14 +24,15 @@ package com.hewei.hzyjy.xunzhi.common.constant;
 public class RedisCacheConstant {
 
     /**
+     * 分组创建分布式锁
+     */
+    public static final String LOCK_GROUP_CREATE_KEY = "xunzhi-agent:lock_group-create:%s";
+
+    /**
      * 用户注册分布式锁
      */
     public static final String LOCK_USER_REGISTER_KEY = "xunzhi-agent:lock_user-register:";
 
-    /**
-     * 分组创建分布式锁
-     */
-    public static final String LOCK_GROUP_CREATE_KEY = "xunzhi-agent:lock_group-create:%s";
 
     /**
      * 用户登录缓存标识
